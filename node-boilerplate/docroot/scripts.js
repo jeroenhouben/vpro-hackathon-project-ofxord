69aaabc21447985e0b286a9e286e2ed01a76a152
Jeroen Houben <jeroen@supercool.nl>
2015-11-23 16:53:32 +0100

    You can now upload local files

--- node-boilerplate/docroot/scripts.js
+++ node-boilerplate/docroot/scripts.js
@@ -1,43 +1,42 @@
-function processFace(imageData) {
-  var params = {
-      // Request parameters
-      "analyzesFaceLandmarks": "true",
-      "analyzesAge": "true",
-      "analyzesGender": "true",
-      "analyzesHeadPose": "true"
-  };
+function uploadToFaceAPI(image) {
+    var params = {
+        // Request parameters
+        "analyzesFaceLandmarks": "true",
+        "analyzesAge": "true",
+        "analyzesGender": "true",
+        "analyzesHeadPose": "true"
+    };
 
-  var url = "/oxford-api-proxy/face/v0/detections?" + $.param(params);
+    var url = "/oxford-api-proxy/face/v0/detections?" + $.param(params);
+    var xhr = new XMLHttpRequest();
+    var fd = new FormData();
 
-  $.ajax({
-      url: url,
-      type: "POST",
-      contentType: "multipart/form-data",
-      data: imageData,
-      dataType: "json"
-  })
-  .done(function(data) {
-      $("#face-api-response").text(JSON.stringify(data));
-  })
-  .fail(function() {
-      alert("error requesting ", url);
-  });
-};
+    xhr.open("POST", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            $("#face-api-response").text(xhr.responseText);
+        }
+    };
+    fd.append('image', image);
+    xhr.send(fd);
+}
 
 $(document).ready(function () {
 
-  $('#upload').on("change", function (e) {
-    var image = e.target.files[0]
 
-    var fileReader = new FileReader();
+    $('#face-api-upload').on("change", function (e) {
+        var image = e.target.files[0];
+        uploadToFaceAPI(image);
 
-    fileReader.onload = (function(file) {
-      return function(e) {
-        processFace(e.target.result);
-      };
-    })(image);
+        var fileReader = new FileReader();
 
-      // Read in the image file as a data URL.
-    fileReader.readAsBinaryString(image);
-  });
+        fileReader.onload = (function(file) {
+            return function(e) {
+                document.getElementById("face-api-upload-preview").src = e.target.result;
+            };
+        })(image);
+
+        // Read in the image file as a data URL.
+        fileReader.readAsDataURL(image);
+    });
 });
--- node-boilerplate/server.js
+++ node-boilerplate/server.js
@@ -3,6 +3,7 @@ var https = require('https');
 var static = require('node-static');
 var fileServer = new static.Server('./docroot');
 var fs = require('fs');
+var formidable = require('formidable');
 
 var API_KEYS = {
   VISION: "72651b4b33a84a2f9676e94511f2de94",
@@ -12,44 +13,47 @@ var API_KEYS = {
 
 var server = http.createServer(function(request, response) {
   if (request.url.indexOf('/oxford-api-proxy') === 0) {
+    var form = new formidable.IncomingForm();
+    form.uploadDir = "./docroot/uploads";
 
-    var localFile = "./DSC_1609.JPG";
-    var localFile = "./pechtold.png";
+    form.parse(request, function(err, fields, files) {
 
-    fs.readFile(localFile, function(err, data) {
-      if (err) {
-          console.log("read jpg fail " + err);
-      } else {
-        var apiRequestOptions = {
-          host: 'api.projectoxford.ai',
-          method: 'POST',
-          data: data,
-          path: request.url.replace('/oxford-api-proxy', ''),
-          headers: {
-            'Content-Type': 'application/octet-stream',
-            'Content-Length': data.length,
-            'Ocp-Apim-Subscription-Key': API_KEYS.FACE
+        fs.readFile(files.image.path, function(err, data) {
+          if (err) {
+            console.log("read jpg fail " + err);
+          } else {
+            var apiRequestOptions = {
+              host: 'api.projectoxford.ai',
+              method: 'POST',
+              data: data,
+              path: request.url.replace('/oxford-api-proxy', ''),
+              headers: {
+                'Content-Type': 'application/octet-stream',
+                'Content-Length': data.length,
+                'Ocp-Apim-Subscription-Key': API_KEYS.FACE
+              }
+            };
+
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+
+            var apiRequest = https.request(apiRequestOptions, function(apiResponse) {
+              var apiResponseText;
+
+              apiResponse.on('data', function(rdata) {
+                response.write(rdata);
+              });
+
+              apiResponse.on('end', function() {
+                response.end();
+              });
+
+            });
+
+            apiRequest.write(data);
+            apiRequest.end();
           }
-        };
-
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-
-        var apiRequest = https.request(apiRequestOptions, function(apiResponse) {
-          var apiResponseText;
-
-          apiResponse.on('data', function(rdata) {
-            response.write(rdata);
-          });
-
-          apiResponse.on('end', function() {
-            response.end();
-          });
-
         });
 
-        apiRequest.write(data);
-        apiRequest.end();
-      }
     });
 
   } else {
